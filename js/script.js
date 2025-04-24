7b7b54c84f1b0ca79f78f5a4ad3dedd96a6c968f
Aimar Fernandez <aimar.fernandez1@stu.ocsb.ca>
2025-04-24 16:58:23 +0000

    added template

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Aimar Fernandez
+// Created on: Feb 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+const positiveNumber = 0
+
+function myButtonClicked() {
+  const positiveNumber = document.getElementById("positive-option").checked
+  if (positiveNumber==true) {
+    const randomNumber = Math.floor(Math.random() * 6) + 1
+  } 
+  else {
+    const randomNumber = Math.floor(Math.random() * -6) + 1
+  }
+  document.getElementById("answer").innerHTML = 
+    "<p>The random number is: </p>" + randomNumber + "</p>"
+}
